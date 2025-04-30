e2b133eb97ac4c1f16bb2378131fa5a117a4c8b2
sualba <su.alba@hotmail.com>
2025-04-30 03:19:23 +0200

    Ejecutar creación de superusuario en Render

--- render_build.sh
+++ render_build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # Archivo para migrar automáticamente en Render
-
+pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
+
+
+python crear_superusuario.py
